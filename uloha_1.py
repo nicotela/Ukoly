e95d4f3b9ba724584737df8a2f26ba22de3a575e
Nicol Götzová <79445545+nicotela@users.noreply.github.com>
2022-01-16 14:00:09 +0100

    Final file v1.0

--- uloha_1.py
+++ uloha_1.py
@@ -1,8 +1,21 @@
-def my_function(input):
-	#implementation of my function, computing of result
-	return result
+def my_function(numbers):
+    even = []
+    odd = []
+    for number in numbers:
+        if (number % 2) == 0:
+            even.append(number)
+        else:
+            odd.append(number)
+    return even, odd
 
-input1 = …….
-input2 = …….
-print(my_function(input1))
-print(my_function(input2))
+input1 = []
+input2 = [1,3,5,35,55,72]
+input3 = [0,2,5,8,23]
+
+sude, liche = my_function(input1)
+print (sude, liche)
+sude, liche = my_function(input2)
+print (sude, liche)
+sude, liche = my_function(input3)
+print (sude, liche)
+ 
