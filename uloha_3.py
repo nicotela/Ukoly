8de8389abbe40dbf5c571f960ba2ee9d86115826
Nicol Götzová <79445545+nicotela@users.noreply.github.com>
2022-01-24 20:15:45 +0100

    Remove duplicity

--- uloha_3.py
+++ uloha_3.py
@@ -1,8 +1,20 @@
-def my_function(input):
-	#implementation of my function, computing of result
-	return result
+def my_function(numbers):
+    unique_numbres = []
+    
+    for number in numbers:
+        if (number in unique_numbres) != True:
+            unique_numbres.append(number)
+        
+    return unique_numbres
 
-input1 = …….
-input2 = …….
-print(my_function(input1))
-print(my_function(input2))
+input1=[ 1,  2, 5, 2, 5, 2, 3, 4, 1, 4, 5]
+input2=[]
+input3=[1,2,3]
+
+
+remove_duplicity = my_function(input1)
+print (remove_duplicity)
+remove_duplicity = my_function(input2)
+print (remove_duplicity)
+remove_duplicity = my_function(input3)
+print (remove_duplicity)
