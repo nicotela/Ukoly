5655ee16e652a7df19db43d7e1049a99e6358177
Nicol Gotzova <nicol.gotzova@gmail.com>
2022-03-14 13:57:45 +0100

    removing whitespaces and adding comment

--- uloha_2.py
+++ uloha_2.py
@@ -1,8 +1,9 @@
-def line_counter (path_to_file):
+def line_counter(path_to_file):
     with open(path_to_file) as f:
         text = f.read()
     count_lines = text.count("\n") + 1
     return count_lines
 
 result = line_counter("/Users/nicolgotzova/Documents/Ukoly/Data/data2.txt")
-print (result)
+print(result)
+#comment
